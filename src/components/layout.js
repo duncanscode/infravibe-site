16fd92398a9f83a1b4ab7ee559a80bcc94c661c0
Duncan Hamilton <duncanhamilton@me.com>
2020-01-26 13:40:22 -0800

    Quick footer fix

--- src/components/layout.js
+++ src/components/layout.js
@@ -14,9 +14,9 @@ import Footer from "./footer"
 const Layout = ({ children }) => {
   return (
     <>
-      <div className="flex flex-col h-full relative">
+      <div className="flex flex-col h-screen">
         <Header />
-        <main className="flex-1 mx-auto p-8">{children}</main>
+        <main className="flex-grow">{children}</main>
         <Footer />
       </div>
     </>
