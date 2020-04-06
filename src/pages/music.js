79f9f017ff5b4c9220300279b249582503a9dd28
huncan <duncanhamilton@me.com>
2020-04-05 20:40:37 -0700

    music page

--- src/pages/music.js
+++ src/pages/music.js
@@ -1,31 +1,71 @@
 import React from "react"
+import { graphql } from "gatsby"
 import Layout from "../components/layout.js"
+import Img from "gatsby-image"
 
-export default () => (
-  <Layout>
-    <div className="container mx-auto px-4 py-10">
-      <div class="flex flex-col">
-        <div class="flex justify-center">
-          <h1 class="text-center text-2xl text-black bg-white font-bold px-4">
-            Latest Releases
-          </h1>
-        </div>
+const Music = ({ data }) => {
+  const { edges } = data.allFile
 
-        <div class="flex flex-col text-gray-700 bg-gray-400 px-4 py-2 m-2">
-          <p class="text-white pb-3">Up To Snow Good Mix</p>
-          <iframe
-            title="Up To Snow Good"
-            width="90%"
-            height="166"
-            scrolling="no"
-            frameborder="no"
-            allow="autoplay"
-            src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/733708690&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"
-          ></iframe>
+  return (
+    <Layout>
+      <div>
+        <div className="container mx-auto px-4 py-10">
+          <div class="flex flex-col">
+            <div class="flex justify-center">
+              <h1 class="text-center text-2xl text-black bg-white font-bold px-4">
+                Latest Releases
+              </h1>
+            </div>
+          </div>
         </div>
-        <div class="text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">2</div>
-        <div class="text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">3</div>
       </div>
-    </div>
-  </Layout>
-)
+
+      <div className="container mx-auto px-4 py-4">
+        <div class="max-w-xs mx-auto px-4">
+          {edges.map(edge => {
+            const { frontmatter } = edge.node.childMarkdownRemark
+            return (
+              <div>
+                <Img fluid={frontmatter.art.childImageSharp.fluid} />
+                <div class="flex items-center justify-between text-white pb-3 font-bold">
+                  {frontmatter.title}
+                  <a href={frontmatter.link}>
+                    <div class="ml-auto italic font-hairline text-sm">
+                      LISTEN
+                    </div>
+                  </a>
+                </div>
+              </div>
+            )
+          })}
+        </div>
+      </div>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query MusicQuery {
+    allFile(filter: { relativeDirectory: { eq: "music" } }) {
+      edges {
+        node {
+          childMarkdownRemark {
+            frontmatter {
+              title
+              link
+              art {
+                childImageSharp {
+                  fluid(maxHeight: 500) {
+                    ...GatsbyImageSharpFluid
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
+export default Music
