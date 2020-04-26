7c7bf5eb9db21e63acbff4aa2e3aa2a4bef8e878
huncan <duncanhamilton@me.com>
2020-04-26 14:53:12 -0700

    stuff

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,26 +4,21 @@ import { graphql, useStaticQuery } from "gatsby"
 import Img from "gatsby-image"
 
 const AboutPage = props => {
-  const data = props.data.allFile.edges[0].node.childMarkdownRemark.frontmatter
+  //const data = props.data.allFile.edges[0].node.childMarkdownRemark.frontmatter
   return (
     <Layout>
       <div>
         <div className="container mx-auto px-4 py-10">
           <div class="flex flex-col">
             <div class="flex justify-center">
-              <button className="bg-transparent border-2 border-white font-hairline text-sm hover:bg-gray-200 hover:text-black hover:font-normal py-4 w-64">
-                {data.title}
-              </button>
+              <button className="bg-transparent border-2 border-white font-hairline text-sm hover:bg-gray-200 hover:text-black hover:font-normal py-4 w-64"></button>
             </div>
           </div>
         </div>
       </div>
 
       <div className="container mx-auto px-4 py-4">
-        <div class="max-w-xs sm:max-w-sm md:max-w-md  lg:max-w-3xl mx-auto px-4">
-          <Img fluid={data.image.childImageSharp.fluid} />
-        </div>
-        <div>{data.description}</div>
+        <div class="max-w-xs sm:max-w-sm md:max-w-md  lg:max-w-3xl mx-auto px-4"></div>
       </div>
     </Layout>
   )
@@ -41,7 +36,7 @@ export const query = graphql`
           childMarkdownRemark {
             frontmatter {
               title
-              description
+
               image {
                 childImageSharp {
                   fluid(maxHeight: 1000) {
